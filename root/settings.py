03e3b50ae6601e3c488f196f86cb00c815f228b9
bobantonbob <130400459+bobantonbob@users.noreply.github.com>
2024-04-30 17:34:36 +0300

    set

--- root/settings.py
+++ root/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY =  env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 
 DEBUG = env('DEBUG')
-ALLOWED_HOSTS = ['final-project-goit-web-antonbabenko.koyeb.app']
+ALLOWED_HOSTS = ['final-project-goit-core-antonbabenko.koyeb.app']
 
 
 # DEBUG = True
